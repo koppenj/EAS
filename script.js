c95cbdd47944da6bf45cd51dc201218040dda39d
kkieninger <ktkienin@gmail.com>
2021-08-13 14:57:48 -0400

    chore: linting / formatting

--- script.js
+++ script.js
@@ -1,54 +1,40 @@
 const container = document.getElementById('grid');
 const resetButton = document.getElementById('reset');
 
-
 function buildGrid(size) {
+    for (let i =1; i <= size; i ++) {
+        for (let j = 1; j <= size; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('gridCell');
 
-    for(let i =1; i <= size; i ++) {
+            container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+            container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-        for(let j = 1; j <=size; j++) {
-
-        const cell = document.createElement('div');
-        cell.classList.add('gridCell');
-
-        container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-        container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-        
-
-        container.appendChild(cell);      
-        
+            container.appendChild(cell);
         }
-    }    
+    }
+
     drawOnGrid();
 }
+
 buildGrid(16);
-
-
-
-
 
 function getRandomColor() {
     let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
     return randomColor;
 }
 
-
 function drawOnGrid() {
-
     let cells = document.querySelectorAll('.gridCell');
-
-	cells.forEach((cell => {
-		cell.addEventListener('mouseover', () => {		
-		    cell.style.backgroundColor = getRandomColor();         
-		})
-		
-	}));
-
+    cells.forEach((cell => {
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = getRandomColor();
+        })
+    }));
 }
 
 
 function drawNewGrid() {
-    
     let newSize = prompt('How big of a canvas do you want?', '16');
 
     if (Number.isNaN(newSize) ) {
@@ -59,23 +45,17 @@ function drawNewGrid() {
     if (newSize > 100 ) {
         alert( " Try and keep it at 100 maximum. ");
         drawNewGrid();
-        
-    } 
-    
-    else {
-        return buildGrid(newSize);
-        
     }
 
+    else {
+        return buildGrid(newSize);
+    }
 }
 
-
 resetButton.onclick = () => {
-
     while(container.firstChild) {
         container.removeChild(container.firstChild);
     }
-    
+
     drawNewGrid();
 }
-
